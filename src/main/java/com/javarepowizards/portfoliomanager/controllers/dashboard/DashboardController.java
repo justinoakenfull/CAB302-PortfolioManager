804fb02e8f829e35cd040e811aed3852abc6b17e
raman <ramanisinghpangly@gmail.com>
2025-04-15 15:54:27 +1000

    added the first widget for the watchlist on dashboard

--- src/main/java/com/javarepowizards/portfoliomanager/controllers/dashboard/DashboardController.java
+++ src/main/java/com/javarepowizards/portfoliomanager/controllers/dashboard/DashboardController.java
@@ -1,4 +1,9 @@
 package com.javarepowizards.portfoliomanager.controllers.dashboard;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 public class DashboardController {
+
+    @FXML
+    private Label watchlistLabel;
 }
