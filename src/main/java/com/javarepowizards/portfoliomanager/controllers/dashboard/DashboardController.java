9753d7a022bdd9b8db9f3c3225d5e010efe6c149
raman <ramanisinghpangly@gmail.com>
2025-04-15 15:54:27 +1000

    added the first widget for the watchlist on dashboard

--- src/main/java/com/javarepowizards/portfoliomanager/controllers/dashboard/DashboardController.java
+++ src/main/java/com/javarepowizards/portfoliomanager/controllers/dashboard/DashboardController.java
@@ -1,18 +1,9 @@
 package com.javarepowizards.portfoliomanager.controllers.dashboard;
 
-import javafx.fxml.Initializable;
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class DashboardController {
 
-
-public class DashboardController implements Initializable {
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-       System.out.println("DashboardController initialized");
-
-        // Run simulation after dashboard loads
-        //SimulationTestRunner testRunner = new SimulationTestRunner();
-        //testRunner.runTestSimulation();
-    }
+    @FXML
+    private Label watchlistLabel;
 }
