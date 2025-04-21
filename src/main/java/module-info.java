3db492c5cd90925bdcd9e31232114367fb28a829
William Hocking <william.m.hocking@gmail.com>
2025-04-21 18:22:23 +1000

    Fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,10 @@ module com.javarepowizards.portfoliomanager {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires spring.beans;
+    requires spring.context;
+    requires java.sql;
+    requires spring.security.crypto;
 
     opens com.javarepowizards.portfoliomanager to javafx.fxml;
     exports com.javarepowizards.portfoliomanager;
@@ -16,5 +20,12 @@ module com.javarepowizards.portfoliomanager {
     exports com.javarepowizards.portfoliomanager.controllers.useraccounts to javafx.fxml;
 
     opens com.javarepowizards.portfoliomanager.controllers.useraccounts to javafx.fxml;
+
+    exports com.javarepowizards.portfoliomanager.controllers.dashboard;
+
+    // Open the controllers.simulation package for reflection by javafx.fxml
+    opens com.javarepowizards.portfoliomanager.controllers.simulation to javafx.fxml;
+
+
 
 }
